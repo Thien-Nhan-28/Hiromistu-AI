69e147804fcf6030bc9710f2af28e63c0eaf3d36
Hiromistu <21130461@st.hcmuaf.edu.vn>
2023-09-28 14:54:39 +0700

    AI

---
+++ sdbg/src/TH1/Action.java
@@ -0,0 +1,5 @@
+package TH1; 
+
+public abstract class Action {
+	public abstract boolean isNoOp();
+}
---
+++ sdbg/src/TH1/ksdf.java
@@ -0,0 +1,8 @@
+package TH1;
+
+public class ksdf {
+	public static void main(String[] args) {
+		System.out.println("jsdgfsbdf");
+	}
+
+}
